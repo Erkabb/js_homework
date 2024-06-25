9250144204c0fa09680b400cb698a1da67b2daa4
erkabb <erkabb816@gmail.com>
2024-06-25 09:37:30 +0800

    f

--- dom/ex.js
+++ dom/ex.js
@@ -1,13 +1,4 @@
 const box = document.getElementById("con");
-// const littleboxes = document.getElementById("box1");
-// const littleboxes2 = document.getElementById("box2");
-// const littleboxes3 = document.getElementById("box3");
-// const littleboxes4 = document.getElementById("box4");
-// const littleboxes5 = document.getElementById("box5");
-// const littleboxes6 = document.getElementById("box6");
-// const littleboxes7 = document.getElementById("box7");
-// const littleboxes8 = document.getElementById("box8");
-// const littleboxes9 = document.getElementById("box9");
 
 const littleboxes = document.getElementsByClassName("box");
 console.log(littleboxes);
@@ -24,14 +15,6 @@ function changeBcolor() {
   for (let i = 0; i < littleboxes.length; i++) {
     littleboxes[i].style.backgroundColor = generatecolor();
   }
-  //   littleboxes2.style.backgroundColor = generatecolor();
-  //   littleboxes3.style.backgroundColor = generatecolor();
-  //   littleboxes4.style.backgroundColor = generatecolor();
-  //   littleboxes5.style.backgroundColor = generatecolor();
-  //   littleboxes6.style.backgroundColor = generatecolor();
-  //   littleboxes7.style.backgroundColor = generatecolor();
-  //   littleboxes8.style.backgroundColor = generatecolor();
-  //   littleboxes9.style.backgroundColor = generatecolor();
 }
 
 btnel.addEventListener("click", changeBcolor);
