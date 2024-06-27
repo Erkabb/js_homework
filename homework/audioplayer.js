77257d02a0f56f6705a11fed4aef1eab8a5b6595
erkabb <erkabb816@gmail.com>
2024-06-27 12:48:45 +0800

    d

--- homework/audioplayer.js
+++ homework/audioplayer.js
@@ -5,18 +5,34 @@ const playaud = document.getElementById("play");
 const pauseaud = document.getElementById("pause");
 const songTitle = document.getElementById("songTitle");
 const songduration = document.getElementById("durationline");
+const artistname = document.getElementById("artist");
+const songIMG = document.getElementById("img");
+let coverimg = "";
 // playbutton.addEventListener("click", () => {
 //   audio.play();
 // });
 
 const playlist = [
   {
+    artist: "Roie Shpigler",
     name: "Aluminum",
     src: "./audio/Roie Shpigler - Aluminum.mp3",
+    coverIMG:
+      "https://images.unsplash.com/photo-1719145177916-1b19e965af72?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4MHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
+    artist: "Ace",
     name: "Pole Position",
     src: "./audio/Ace - Pole Position.mp3",
+    coverIMG:
+      "https://images.unsplash.com/photo-1719244720568-40136cd0b36e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5Mnx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    artist: "Rex Banner",
+    name: "Human",
+    src: "./audio/Rex Banner - Human.mp3",
+    coverIMG:
+      "https://images.unsplash.com/photo-1719242086474-426087b8e0d3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5NHx8fGVufDB8fHx8fA%3D%3Ds",
   },
 ];
 
@@ -27,7 +43,7 @@ playbutton.addEventListener("click", () => {
   if (audio.paused) {
     audio.play();
     playbutton.innerHTML = `<i id="pause" class="fa-solid fa-pause fa-2xl hide"></i>`;
-    song;
+
     songTitle.textContent = playlist[currentsong].name;
     playaud.classList.add("hide");
     pauseaud.classList.remove("hide");
@@ -35,8 +51,8 @@ playbutton.addEventListener("click", () => {
     audio.pause();
     playbutton.innerHTML = `<i id="play" class="fa-solid fa-play fa-2xl"></i>`;
 
-    playaud.classList.remove("hide");
-    pauseaud.classList.add("hide");
+    // playaud.classList.remove("hide");
+    // pauseaud.classList.add("hide");
   }
   console.log("audio:", audio);
 });
@@ -52,14 +68,20 @@ nextbtn.addEventListener("click", () => {
     currentsong = 0;
   }
   audio.src = playlist[currentsong].src;
+
   songTitle.textContent = playlist[currentsong].name;
+  artistname.textContent = playlist[currentsong].artist;
+  songIMG.style.backgroundImage = "url()";
+
   audio.play();
+
+  playbutton.innerHTML = `<i id="pause" class="fa-solid fa-pause fa-2xl hide"></i>`;
 });
-songduration = (aud) => {
-  aud = audio.duration;
-  return aud;
-}
-songduration(audio.length);
+// songduration = (aud) => {
+//   aud = audio.duration;
+//   return aud;
+// };
+// songduration(audio.length);
 // const songduration=()=>{
 //   alert(audio.duration);
 // }
